fd848eefe11c406e3eff12f0013f8ce81aff927c
margenta19 <33258602+margenta19@users.noreply.github.com>
2018-03-01 10:57:04 -0600

    added the x variable

--- 13.1.2.py
+++ 13.1.2.py
@@ -2,13 +2,16 @@ import setup
 from setup import RPL
 import post_to_web as PTW # see post_to_web.py for instructions
 import time
+import RoboPiLib as RPL
 #time.time- start equal to a value- can have it equal to the end of the program- to find the elapsed time- current time.time
 #subtracted by the start time.time is divided- how many leftovers there are when that equals 0 the robot would stop
 start = time.time()
 
+x = 4
+
 while True:
-    elaptime= (time.time - start)
-    while x=0 = (0, 0)
-    while x=1 = (2, 500)
+    elaptime= (time.time - start) / x
+        while x=0 = (0, 0)
+        while x=1 = (2, 500)
     
     
