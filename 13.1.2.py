5fc7ea7da0643660f48b35ffced63703fe206650
margenta19 <33258602+margenta19@users.noreply.github.com>
2018-03-01 10:53:16 -0600

    Update 13.1.2.py

--- 13.1.2.py
+++ 13.1.2.py
@@ -1,5 +1,12 @@
 import setup
 from setup import RPL
 import post_to_web as PTW # see post_to_web.py for instructions
-
 import time
+#time.time- start equal to a value- can have it equal to the end of the program- to find the elapsed time- current time.time
+#subtracted by the start time.time is divided- how many leftovers there are when that equals 0 the robot would stop
+start = time.time()
+
+while True:
+    elaptime= (time.time - start)
+    while 0 = (0, 0)
+    
