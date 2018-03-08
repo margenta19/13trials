dc3c41f6334556fe90944c9dd4cf00e1b512c613
margenta19 <33258602+margenta19@users.noreply.github.com>
2018-03-08 10:11:04 -0600

    Update 13.1.1.py

--- 13.1.1.py
+++ 13.1.1.py
@@ -10,13 +10,13 @@ while True:
   PTW.post()
 
   if RPL.digitalRead(sensor_pin) == 1:
-      import RoboPiLib as RPL
-      import setup
-      RPL.servoWrite(2, 500)
-      RPL.servoWrite(1, 2000)
+     import RoboPiLib as RPL
+     import setup
+     RPL.servoWrite(2, 500)
+     RPL.servoWrite(1, 2000)
     
   if RPL.digitalRead(sensor_pin) == 0:
-      import RoboPiLib as RPL
-      import setup
-      RPL.servoWrite(2, 0)
-      RPL.servoWrite(1,0)
+     import RoboPiLib as RPL
+     import setup
+     RPL.servoWrite(2, 0)
+     RPL.servoWrite(1,0)
