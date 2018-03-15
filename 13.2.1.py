09dacb8287edba98e8e0f1b901c2a2eebb448d01
margenta19 <33258602+margenta19@users.noreply.github.com>
2018-03-15 10:33:12 -0500

    Create 13.2.1.py

---
+++ 13.2.1.py
@@ -0,0 +1,22 @@
+import setup
+from setup import RPL
+import post_to_web as PTW
+
+sensor_pin = 17
+RPL.pinMode(sensor_pin, RPL.INPUT)
+
+while True:
+  PTW.state['d1'] = RPL.digitalRead(sensor_pin)
+  PTW.post()
+
+  if RPL.digitalRead(sensor_pin) == 1:
+     import RoboPiLib as RPL
+     import setup
+     RPL.servoWrite(2,500)
+     RPL.servoWrite(1,2000)
+    
+  if RPL.digitalRead(sensor_pin) == 0:
+     import RoboPiLib as RPL
+     import setup
+     RPL.servoWrite(2,0)
+     RPL.servoWrite(1,0)
