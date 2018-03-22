8451fb19b81a5c60ff21f1cacf4b031da7851ec7
margenta19 <33258602+margenta19@users.noreply.github.com>
2018-03-22 11:09:21 -0500

    Update stop.py

--- stop.py
+++ stop.py
@@ -1,4 +1,22 @@
-import RoboPiLib as RPL
 import setup
-RPL.servoWrite(0,0)
-RPL.servoWrite(1,0)
+from setup import RPL
+import post_to_web as PTW # see post_to_web.py for instructions
+import time
+import RoboPiLib as RPL
+
+start = time.time()
+
+x = 1
+
+while True:
+    elaptime = (time.time() - start)
+    x = 0
+    elaptime = int(elaptime)
+    if elaptime % 6 == 0:
+        RPL.servoWrite(1,0)
+        RPL.servoWrite(2,0)
+        x = x + 12
+    if elaptime % 12 == 0:
+        RPL.servoWrite(1,100)
+        RPL.servoWrite(2,2000)
+        x = x + 6
