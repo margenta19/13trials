b4091065c9d8d54ab0277ca49708dcbef6b8f750
margenta19 <33258602+margenta19@users.noreply.github.com>
2018-04-09 11:02:57 -0500

    Update 13.2.2.py

--- 13.2.2.py
+++ 13.2.2.py
@@ -12,11 +12,11 @@ while True:
   if RPL.digitalRead(sensor_pin) == 1:
      import RoboPiLib as RPL
      import setup
-     RPL.servoWrite(1,2000)
-     RPL.servoWrite(2,500)
+     RPL.servoWrite(1,500)
+     RPL.servoWrite(2,2000)
     
   if RPL.digitalRead(sensor_pin) == 0:
      import RoboPiLib as RPL
      import setup
-     RPL.servoWrite(2,250)
-     RPL.servoWrite(1,1000)
+     RPL.servoWrite(2,1000)
+     RPL.servoWrite(1,250)
