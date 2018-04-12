398bdc66234f7557d86dd9b36ddf2da951754400
margenta19 <33258602+margenta19@users.noreply.github.com>
2018-04-12 11:10:03 -0500

    Update 13.2.2.py

--- 13.2.2.py
+++ 13.2.2.py
@@ -16,7 +16,26 @@ while True:
      RPL.servoWrite(2,2000)
     
   if RPL.digitalRead(sensor_pin) == 0:
-     import RoboPiLib as RPL
      import setup
-     RPL.servoWrite(2,1000)
-     RPL.servoWrite(1,250)
+     from setup import RPL
+     import post_to_web as PTW # see post_to_web.py for instructions
+     import time
+     import RoboPiLib as RPL
+#time.time- start equal to a value- can have it equal to the end of the program- to find the elapsed time- current time.time
+#subtracted by the start time.time is divided- how many leftovers there are when that equals 0 the robot would stop
+     start = time.time()
+
+       x = 1
+
+while True:
+    elaptime = (time.time() - start)
+    x = 0
+    elaptime = int(elaptime)
+    if elaptime % 1 == 0:
+        RPL.servoWrite(1,0)
+        RPL.servoWrite(2,0)
+        x = x + 2
+    if elaptime % 2 == 0:
+        RPL.servoWrite(2,1000)
+        RPL.servoWrite(1,250)
+        x = x + 1
