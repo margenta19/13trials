c3061ed8a4fac965709fa4be8ef97cf7a9064337
margenta19 <33258602+margenta19@users.noreply.github.com>
2018-04-30 11:03:49 -0500

    Update rightturn3.py

--- rightturn3.py
+++ rightturn3.py
@@ -4,16 +4,27 @@ import post_to_web as PTW # see post_to_web.py for instructions
 
 sensor_pin = 16
 RPL.pinMode(sensor_pin,RPL.INPUT)
-  
-while True:
-    elapsed = time.time() - start
-    y = 2
-    
-if int(elapsed) != 0:
-    y = 4
-    while int(elapsed) % 2 == 0:
-      RPL.servoWrite(2,0)
-      RPL.servoWrite(1,0)
-      quit()
 
+      while True:
+       elapsed = time.time() - start
+       y = 1
+        
+       if int(elapsed) != 0:
+         y = 2
+         while int(elapsed) % 2 == 0:
+           RPL.servoWrite(2,2000)
+           RPL.servoWrite(1,500)
+
+      
+      
+      while True:
+        elapsed = time.time() - start
+        y = 2
+        
+        if int(elapsed) != 0:
+          y = 4
+          while int(elapsed) % 2 == 0:
+            RPL.servoWrite(2,0)
+            RPL.servoWrite(1,0)
+            quit()
 
