d260880da9b3d6c25ee46ade72a806fd86a638f2
margenta19 <33258602+margenta19@users.noreply.github.com>
2018-04-30 11:02:03 -0500

    Update rightturn3.py

--- rightturn3.py
+++ rightturn3.py
@@ -9,11 +9,11 @@ while True:
     elapsed = time.time() - start
     y = 2
     
-    if int(elapsed) != 0:
+if int(elapsed) != 0:
     y = 4
-       while int(elapsed) % 2 == 0:
-       RPL.servoWrite(2,0)
-       RPL.servoWrite(1,0)
+    while int(elapsed) % 2 == 0:
+    RPL.servoWrite(2,0)
+    RPL.servoWrite(1,0)
        quit()
 
 
