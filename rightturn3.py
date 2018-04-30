d2d99b1d274f51e1aa985f3a9960a1acf7714aec
margenta19 <33258602+margenta19@users.noreply.github.com>
2018-04-30 11:08:30 -0500

    Update rightturn3.py

--- rightturn3.py
+++ rightturn3.py
@@ -4,27 +4,34 @@ import post_to_web as PTW # see post_to_web.py for instructions
 
 sensor_pin = 16
 RPL.pinMode(sensor_pin,RPL.INPUT)
-
-if RPL.digitalRead(sensor_pin) == 0:
+     
+  if RPL.digitalRead(sensor_pin) == 0:
      import RoboPiLib as RPL
      import setup
      RPL.servoWrite(0,0)
      RPL.servoWrite(1,0)
      x = 0
-
-     while True:
-          elapsed = time.time() - start
-          y = 1
-        
-     if int(elapsed) != 0:
-       y = 2
-       while int(elapsed) % 2 == 0:
-         RPL.servoWrite(2,2000)
-         RPL.servoWrite(1,500)
+      
+     while x == 0:
+      import time
+      import setup
+      from setup import RPL
+      import RoboPiLib as RPL
+      start = time.time()
           
-     while True:
-          elapsed = time.time() - start
-          y = 2
+      while True:
+       elapsed = time.time() - start
+       y = 1
+        
+       if int(elapsed) != 0:
+         y = 2
+         while int(elapsed) % 2 == 0:
+           RPL.servoWrite(2,2000)
+           RPL.servoWrite(1,500)
+
+      while True:
+        elapsed = time.time() - start
+        y = 2
         
         if int(elapsed) != 0:
           y = 4
@@ -32,4 +39,3 @@ if RPL.digitalRead(sensor_pin) == 0:
             RPL.servoWrite(2,0)
             RPL.servoWrite(1,0)
             quit()
-
